4266aa5f6e96248a4ed7439be920d90e6ef439c8
lxk613 <lxk613@163.com>
2015-08-03 16:32:24 +0800

    update

--- decoratorUsageExaple.py
+++ decoratorUsageExaple.py
@@ -1,18 +1,18 @@
-import functools #part of python standard lib
-
-
-def decorator(wapped_function):
-	def _wrapper(*args,**kwargs):
-        #do something beore the function call
-        result = wrapped_function(*args,**kwargs)
+import functools #part of python standard lib, it can fix the decoratored function,because doc string would missing after decoratored
+#http://www.pydanny.com/python-decorator-cheatsheet.html
+#https://codefisher.org/catch/blog/2015/02/10/python-decorators-and-context-managers/
+#https://wiki.python.org/moin/PythonDecoratorLibrary
+def decorator(wrapped_function):
+    def _wrapper(*args,**kwargs):
+        result=wrapped_function(*args,**kwargs)
         #do sth after the function call
         return result
-    return _wapper
-    
+    return _wrapper
+
 
 #decorator with functools.wapps added
-def decorator_with_waps(wrapped_function):
-    @functools.waps(wrapped_function)
+def decorator_with_wraps(wrapped_function):
+    @functools.wraps(wrapped_function)
     def _wrapper(*args,**kwargs):
         #do sth before function call
         result = wrapped_function(*args,**kwargs)
@@ -20,7 +20,8 @@ def decorator_with_waps(wrapped_function):
         return result
     return _wrapper
 
-import wappt # require installing the wrapt library
+"""
+import wrappt # require installing the wrapt library
 #decorator powered by wrapt
 @wrapt.decorator
 def decorator_with_wrapt(wrapped_function,instance,args,kwargs):
@@ -28,20 +29,22 @@ def decorator_with_wrapt(wrapped_function,instance,args,kwargs):
 	result = wrapped_function(*args,**kwargs)
 	#do sth after the function call
 	return result
-
+"""
 def test_decorators_without_args():
-	@decorator
-	def func1():
-		return 'I'
+    @decorator
+    def func1():
+        return 'I'
     @decorator_with_wraps
     def func2():
     	return 'code'
+    """
     @decorator_with_wappt
     def func3():
     	return 'python'
+    """
     assert func1() == 'I'
     assert func2() == 'code'
-    assert func3() == 'python'
+    #assert func3() == 'python'
     
 #decorators with arguments example
 def arg_decorator(arg1,arg2):
@@ -51,9 +54,9 @@ def arg_decorator(arg1,arg2):
             result = wrapped_function(*args,**kwargs)
             #do sth after the function call
             #demostrating what you can do with decorator arguments
-            result = result*arg*arg2
+            result = result*arg1*arg2
             return result
-        return _wapper
+        return _wrapper
     return _outer_wrapper
 
 def arg_decorator_with_wraps(arg1,arg2):
@@ -66,9 +69,10 @@ def arg_decorator_with_wraps(arg1,arg2):
             #demonstrating what you ca ndo with decorator args
             result = result *arg1 * arg2
             return result
-        return _wapper
-    return _out_wapper
+        return _wrapper
+    return _outer_wrapper
 
+"""
 def arg_decorator_with_wapt(arg1,arg2):
     @wrapt.decorator
     def _wapper(wrapper_function,instance,args,kwargs):
@@ -79,7 +83,7 @@ def arg_decorator_with_wapt(arg1,arg2):
         result = result *arg1 *arg2
         return result
     return _wapper
-
+"""
 def test_args_decorators_with_args():
     @arg_decorator(2,3)
     def func4():
@@ -87,14 +91,15 @@ def test_args_decorators_with_args():
     @arg_decorator_with_wraps(2,2)
     def func5():
         return 'test'
+    """
     @arg_decorator_with_wrapt(2,3)
     def func6():
         return 'py'
-    
+    """
 
     assert func4() =='wewewewewewe'
     assert func5() =='testtesttesttest'
-    assert func6() == 'pypypypypypy'
+    #assert func6() == 'pypypypypypy'
     
 if __name__ == '__main__':
     test_decorators_without_args()
